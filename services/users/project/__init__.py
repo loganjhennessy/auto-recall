70e97dd7bc5040aaf379c08aab7258417a8a7286
Logan Hennessy <loganjhennessy@gmail.com>
2018-12-02 09:47:22 -0800

    Chapter 6 checkin. Database set up.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 # services/users/project/__init__.py
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 import os
 
@@ -13,6 +14,22 @@ CORS(app)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 @app.route('/users', methods=['GET'])
