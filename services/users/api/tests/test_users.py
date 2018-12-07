1a1d0eb99a9346db3c22b1a026851abede403bc9
Logan Hennessy <loganjhennessy@gmail.com>
2018-12-07 08:56:50 -0800

    Add tests for error cases

--- services/users/api/tests/test_users.py
+++ services/users/api/tests/test_users.py
@@ -28,3 +28,52 @@ class TestUserService(object):
         assert response.status_code == 201
         assert 'loganjhennessy@gmail.com was added!' in data['message']
         assert 'success' in data['status']
+
+    def test_add_user_invalid_json(self, client_fixture):
+        """Ensure error is thrown if the JSON object is empty."""
+        response = client_fixture.post(
+            '/users',
+            data=json.dumps({}),
+            content_type='application/json',
+        )
+        data = json.loads(response.data.decode())
+        assert response.status_code == 400
+        assert 'Invalid payload' in data['message']
+        assert 'fail', data['status']
+
+    def test_add_user_invalid_json_keys(self, client_fixture):
+        """
+        Ensure error is thrown if the JSON object does not have a username keyself.
+        """
+        response = client_fixture.post(
+            '/users',
+            data=json.dumps({'email': 'loganjhennessy@gmail.com'}),
+            content_type='application/json',
+        )
+        data = json.loads(response.data.decode())
+        assert response.status_code == 400
+        assert 'Invalid payload.' in data['message']
+        assert 'fail', data['status']
+
+    def test_add_user_duplicate_email(self, client_fixture, database_fixture):
+        """Ensure error is thrown if the email already exists."""
+        client_fixture.post(
+            '/users',
+            data=json.dumps({
+                'username': 'logan',
+                'email': 'loganjhennessy@gmail.com'
+            }),
+            content_type='application/json',
+        )
+        response = client_fixture.post(
+            '/users',
+            data=json.dumps({
+                'username': 'logan',
+                'email': 'loganjhennessy@gmail.com'
+            }),
+            content_type='application/json',
+        )
+        data = json.loads(response.data.decode())
+        assert response.status_code == 400
+        assert 'Sorry. That email already exists.' in data['message']
+        assert 'fail', data['status']
